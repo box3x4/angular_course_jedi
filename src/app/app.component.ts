b20af79a85131beb6233eafabccdd89303b8e2ce
box3x4 <bruno.carvalho1505@hotmail.com>
2018-08-07 21:42:34 -0300

    primeiro commit curso angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { Student } from "./student/student.model";
 
 @Component({
-  selector: 'jad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "jad-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  students: Student[] = [
+    { name: "luke", isJedi: true, temple: "Coruscant" },
+    { name: "leia", isJedi: false },
+    { name: "han solo", isJedi: false }
+  ];
 }
